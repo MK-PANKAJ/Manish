5af27f1cf27369704e75685e15e59d9c4923a610
MK-PANKAJ <manishkumarpankaj119@gmail.com>
2024-10-28 16:47:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,9 +57,9 @@ noButton.addEventListener('click', () => {
     customMessage.textContent = messages[randomIndex];
     
     // If the user clicks No 5 times, show a special message
-    if (noButtonClickCount >= maxNoClicks)
-{noButton.disabled = true
-       noButton.style.transform = `scale(${scale*0.01})`
- customMessage.textContent = "Okay, I get it. You're a tough nut to crack! 😜";
+    if (noButtonClickCount >= maxNoClicks) {
+        noButton.disabled = true;
+        noButton.style.transform = `scale(${scale * 0.01})`; // Scale down the button
+        customMessage.textContent = "Okay, I get it. You're a tough nut to crack! 😜";
     }
 });
